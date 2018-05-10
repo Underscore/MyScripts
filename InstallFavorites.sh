038d1b5a9d00b08c77c431955d91798ab37f3d43
Underscore <yodaguy5@gmail.com>
2018-05-09 22:31:39 -0500

    Cleanup and changes. Now broken at the error log and increment.

--- InstallFavorites.sh
+++ InstallFavorites.sh
@@ -3,24 +3,23 @@
 #Setup
 PATH="$PATH:/usr/games"
 apt-get update
-pip install --upgrade pip
+#pip install --upgrade pip
 
 #Set variables
-pkgname=(terminator vim git ruby cowsay fortune-mod vlc nmap nyancat figlet toilet screen tmux)
-retries=5
+pkgname=(terminator vim git ruby cowsay fortune-mod vlc nmap nyancat figlet toilet screen tmux gimp)
+retries=1
+max=5
 
 #Create function to install and retry up to 5 times on failure
 function installandretry() {
-for i in ${pkgname[@]}; do #Loop through all pkgs in the array
-  if [ "$retries" -ge 0 ]; then #If there are retries left continue
-    apt-get install $i -y && continue #install from list auto yes and continue
+for i in ${pkgname[@]}; do 
+  if [ "$retries" -le 5 ]; then 
+    apt-get install $i -y && continue 
   else
-   retries=`expr $retries - 1` #if install failed dec counter
-   touch errors.txt #create error log
-   echo "$i failed to install on $(date)" >> errors.txt #add to error log with name and date 
-  fi # exit if
-  retries=5 #reinitialize retry variable
-done #exit for loop
+    echo "$i failed to install $retries out of $max times on $(date)." >> errors.txt
+  fi
+  retries=1 
+done
 }
 
 #Execute function to install pkgs with up to 5 retries. 
@@ -29,27 +28,28 @@ set -xeuo
 installandretry
 set +x
 
-#Install lolcat
-##Python 2.7 version
-##pip install lolcat -y
-
-##Ruby Version
-wget https://github.com/busyloop/lolcat/archive/master.zip
-unzip master.zip
-cd lolcat-master/bin
-gem install lolcat
-cd ..
-cd ..
-rm -rf lolcat-master
-rm -rf master.zip
-
-#Upgrade any out of data packages
-apt-get upgrade -y
+##Install lolcat
+###Python 2.7 version
+###pip install lolcat -y
+#
+###Ruby Version
+#wget https://github.com/busyloop/lolcat/archive/master.zip
+#unzip master.zip
+#cd lolcat-master/bin
+#gem install lolcat
+#cd ..
+#cd ..
+#rm -rf lolcat-master
+#rm -rf master.zip
+#
+##Upgrade any out of data packages
+#apt-get upgrade -y
 
 #Completion message
-if dkpg -s lolcat > /dev/null 2>&1; && dpkg -s cowsay >/dev/null 2>&1; then
+if dkpg -s lolcat >/dev/null 2>&1 && dpkg -s cowsay >/dev/null 2>&1; then
   cowsay -f tux Packages have finished installing. | lolcat
 else 
   echo "Packages have finished installing!"
 fi
+
 
